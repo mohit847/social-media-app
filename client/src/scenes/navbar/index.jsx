92010f40136fa95af4d12d2fd0fe17d59d372c46
Mohit Kumar <94470790+mohit847@users.noreply.github.com>
2023-01-02 16:16:56 +0530

    commit 2

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
 >
-          Sociopedia
+          Sociocircle
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
